4ab43911a505679fbf2857ea6f7294bad91ac763
sunmi92 <vbo3129@naver.com>
2021-02-06 23:20:25 +0900

    commit

--- node/main.js
+++ node/main.js
@@ -55,7 +55,12 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist);
             var template = templateHTML(title, list,
               `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+              ` <a href="/create">create</a>
+                <a href="/update?id=${title}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <input type="submit" value="delete">
+                </form>`
             );
             response.writeHead(200);
             response.end(template);
@@ -92,7 +97,7 @@ var app = http.createServer(function(request,response){
           fs.writeFile(`data/${title}`, description, 'utf8', function(err){
             response.writeHead(302, {Location: `/?id=${title}`});
             response.end();
-          });
+          })
       });
     } else if(pathname === '/update'){
       fs.readdir('./data', function(error, filelist){
@@ -132,8 +137,21 @@ var app = http.createServer(function(request,response){
             fs.writeFile(`data/${title}`, description, 'utf8', function(err){
               response.writeHead(302, {Location: `/?id=${title}`});
               response.end();
-            });
+            })
           });
+      });
+    } else if(pathname === '/delete_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          fs.unlink(`data/${id}`, function(error){
+            response.writeHead(302, {Location: `/`});
+            response.end();
+          })
       });
     } else {
       response.writeHead(404);
