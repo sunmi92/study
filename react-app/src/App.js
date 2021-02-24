8c0bace4cd51480b3ddd188c6e50cd96040048b0
sunmi92 <vbo3129@naver.com>
2021-02-24 23:27:28 +0900

    onChangePage

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode: 'read',
+      mode: 'welcome',
+      selected_content_id: 1,
       subject:{title:'WEB', sub:'World Wide Web!'},
       welcome:{title:'Welcome', desc:'Hello, React!'},
       contents: [
@@ -25,27 +26,40 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if(this.state.mode === 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
 
     return (
       <div className="App">
-      {/*<Subject
+      <Subject
         title={this.state.subject.title}
-        sub={this.state.subject.sub}>
-      </Subject> */}
-      <header>
-        <h1><a href='/' onClick={function(e) {
-          console.log(e);
-          e.preventDefault();
+        sub={this.state.subject.sub}
+        onChangePage={function() {
           this.setState({
-            mode:'welcome'
-          });
-        }.bind(this)}>{this.state.subject.title}</a></h1>
-        {this.state.subject.sub}
-      </header>
-      <TOC data={this.state.contents}></TOC>
+            mode:'welcome',
+            selected_content_id:0
+          })
+        }.bind(this)}>
+      </Subject>
+
+      <TOC
+        onChangePage={function(id){
+          this.setState({
+            mode:'read',
+            selected_content_id:Number(id)
+          })
+      }.bind(this)}
+      data={this.state.contents}
+      ></TOC>
       <Contents title={_title} desc={_desc}></Contents>
       </div>
     );
